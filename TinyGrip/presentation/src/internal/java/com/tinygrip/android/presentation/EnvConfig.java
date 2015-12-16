b328fd8058aaa6b7c680f9cd28da15a8f7072967
Javier Tarazaga <javier.tarazaga@neonstingray.com>
2015-12-16 17:39:02 +0100

    Added EnvConfig and buildTypes

---
+++ TinyGrip/presentation/src/internal/java/com/tinygrip/android/presentation/EnvConfig.java
@@ -0,0 +1,9 @@
+package com.tinygrip.android.presentation;
+
+/**
+ * Simple enviromental configuration file for each product buildType
+ */
+public class EnvConfig {
+    public static final boolean DEBUG = true;
+    public static final boolean USE_TEST_CREDS = true;
+}
---
+++ TinyGrip/presentation/src/release/java/com/tinygrip/android/presentation/EnvConfig.java
@@ -0,0 +1,9 @@
+package com.tinygrip.android.presentation;
+
+/**
+ * Simple enviromental configuration file for each product buildType
+ */
+public class EnvConfig {
+    public static final boolean DEBUG = false;
+    public static final boolean USE_TEST_CREDS = false;
+}
